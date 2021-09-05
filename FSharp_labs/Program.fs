713c8748f73c95721fe786f012715c2a55c841c2
nickiteks <55086354+nickiteks@users.noreply.github.com>
2021-09-05 10:10:41 +0400

    Initial commit

---
+++ FSharp_labs/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+let main(number: int) =   
+      let mutable sum = 0
+      for i in 1..number do
+            if i % 3 > 0 && i % 5 = 0  then
+                  sum <- sum + i
+                  printfn "Number %i Sum = %i" i sum
+      printf "Result: %i" sum
+            
+main(100)
+
+Console.ReadKey() |> ignore
